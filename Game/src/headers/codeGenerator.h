b85baf0f5bd7070d5e1c96d87fd66f7914183e90
terrorcftmg <jvcdef@gmail.com>
2018-11-12 21:48:24 -0200

    Estrutura gameCode movida para arquivo gameCode.h separado

--- Game/src/headers/codeGenerator.h
+++ Game/src/headers/codeGenerator.h
@@ -1,16 +1,4 @@
-/**
- * Estrutura gameCode contendo
- * tamanho da senha, limite máximo,
- * se permite repetições e o valor
- * gerado pela função generateCode.
-*/
-typedef struct gameCode{
-    int length;
-    int maxRange;
-    int allowRepeat;
-    char *codeValue;
-} gameCode;
-
+#include "gameCode.h"
 
 gameCode *getCode();
 
---
+++ Game/src/headers/gameCode.h
@@ -0,0 +1,12 @@
+/**
+ * Estrutura gameCode contendo
+ * tamanho da senha, limite máximo,
+ * se permite repetições e o valor
+ * gerado pela função generateCode.
+*/
+typedef struct gameCode{
+    int length;
+    int maxRange;
+    int allowRepeat;
+    char *codeValue;
+} gameCode;
